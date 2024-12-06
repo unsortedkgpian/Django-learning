600eaa0c83a5055f291a0d2411bd102c9fa7ef88
unsortedkgpian <theadityakumar2810@gmail.com>
2024-12-06 19:54:43 +0530

    relationship models

--- django01/atomicapp/admin.py
+++ django01/atomicapp/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
-from .models import DanjoVarity
+from .models import DanjoVarity, atomReviews,Store,AtomCertification
+
+
 # Register your models here.
-admin.site.register(DanjoVarity)
+class ChaiReviewInline(admin.TabularInline):
+    model = atomReviews
+    extra = 2
+    
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type, date_added')
+    inlines = [ChaiReviewInline]
+    
+class StoreAdmin(admin.ModelAdmin):
+    list_display=('name', 'location')
+    filter_horizontal=('chai_varieties',)
+
+class ChaiCetificateAdmin(admin.ModelAdmin):
+    list_display=('chai', 'certificate_number')
+
+
+
+admin.site.register(DanjoVarity,ChaiVarietyAdmin)
--- django01/atomicapp/models.py
+++ django01/atomicapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 class DanjoVarity(models.Model):
@@ -21,5 +21,35 @@ class DanjoVarity(models.Model):
     def __str__(self) :
         return self.name
 
+
+# one to many
+class atomReviews(models.Model):
+    atom = models.ForeignKey(DanjoVarity , on_delete=models.CASCADE, related_name='rewies')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment=models.IntegerField()
+    date_added = models.DateTimeField(default= timezone.now)
+
+    def __str__(self) :
+        return f'{self.user.username} review for {self.chai.name}'
+
+## Many to Many
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    chai_varieties = models.ManyToManyField(DanjoVarity, related_name='stores')
+
+    def __str__(self) :
+        return self.name
     
     
+#One to one
+class AtomCertification(models.Model):
+    atom = models.OneToOneField(DanjoVarity, on_delete=models.CASCADE, related_name='certificate')
+    certicate_number = models.CharField(max_length=100)
+    issued_date = models.DateTimeField(default=timezone.now)
+    valid_untill = models.DateTimeField()
+    
+    def __str__(self) :
+        return self.name.atom
